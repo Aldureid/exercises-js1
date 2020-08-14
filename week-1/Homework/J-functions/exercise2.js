a745371e89db5a0a2cb5eba6a035256f9e3f7b66
Billy Everyteen <aldouraid@gmail.com>
2020-08-14 21:26:13 +0200

    numbers

--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,8 @@
-function triple(4) {
-  // complete function here
-}
-
+function triple(number) {return number * 2;}
 var result = triple(12);
-
 console.log(result);
+
+function triple(number) {return number * 5;}
+var result = triple(10);
+console.log(result);
+
