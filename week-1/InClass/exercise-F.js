212530e6ecdf885fc7a8fced56a3e8039adaa936
Billy Everyteen <aldouraid@gmail.com>
2020-08-15 12:03:11 +0200

    round

--- week-1/InClass/exercise-F.js
+++ week-1/InClass/exercise-F.js
@@ -1,7 +1,7 @@
 const numberOfStudents = 15;
 const numberOfMentors = 8;
-const hay = "there are "
-const hay2 = " persons"
+const hay = "there are ";
+const hay2 = " persons";
 const sum = numberOfStudents + numberOfMentors
 const number = hay + sum +hay2
 console.log(number); 
---
+++ week-1/InClass/exercise-G.js
@@ -0,0 +1,6 @@
+let PercentageStudents = "64.7";
+let PercentageMentors = "35.3";
+let students = Math.round(PercentageStudents);
+let mentors = Math.round(PercentageMentors);
+console.log(students);
+console.log(mentors);
